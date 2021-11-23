96ac02f77f1cf7f32568fb08ec772a55b441d6b6
Chuks Akibot <juniorakibor@yahoo.com>
2021-11-23 11:40:37 +0100

    posts created with scully

--- my-blog/src/app/app-routing.module.ts
+++ my-blog/src/app/app-routing.module.ts
@@ -6,10 +6,7 @@ const routes: Routes = [
   { path: 'contact', component: ContactComponent },
   { path: 'articles', loadChildren: () => import('./articles/articles.module').then(m => m.ArticlesModule) },
   { path: '', pathMatch: 'full', redirectTo: 'articles' },
-<<<<<<< HEAD
   { path: 'posts', loadChildren: () => import('./posts/posts.module').then(m => m.PostsModule) },
-=======
->>>>>>> f7dfc5dcc7a0f16e78cd6ef945473a79d9ca70cd
   { path: '**', redirectTo: 'articles' }
 ];
 
